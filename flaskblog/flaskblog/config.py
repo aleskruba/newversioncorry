834764a05aba7846523a5acf39282b8a2acdbb0d
aleskruba <74367962+aleskruba@users.noreply.github.com>
2021-02-07 19:12:08 +0100

    Delete config.py

--- flaskblog/flaskblog/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    'SECRET_KEY' = 'aa619dbda413f99de65fde9b865605c5'
-    'SQLALCHEMY_DATABASE_URI' = 'sqlite:///correy.db'
-    'MAIL_SERVER' = 'smtp.centrum.cz'
-    'MAIL_PORT' = 587
-    'MAIL_USE_TLS' = True
-    'MAIL_USERNAME' = 'aleskruba@centrum.cz'
-    'MAIL_PASSWORD' = 'Adamek2003'
